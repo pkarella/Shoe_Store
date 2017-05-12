d3584cc2b978588ebcb0c6767230680e6513f5f8
peter <pkarella27@yahoo.com>
2017-05-12 16:22:00 -0700

    refactoring two

--- app.rb
+++ app.rb
@@ -17,20 +17,12 @@ end
 post("/stores") do
   @stores = Store.all()
   store_name = params.fetch("store_name")
-  Store.create({:store_name => store_name})
+  @store= Store.create({:store_name => store_name})
   erb(:stores_page)
 end
 
-get("/stores/:id")do
+get("/stores/:id/edit")do
   @store = Store.find(params.fetch("id").to_i())
-  erb(:store_info)
-end
-
-post("/brands")do
-  shoe_name = params.fetch("shoe_name")
-  store_id = params.fetch("store_id").to_i()
-  @store = Store.find(params.fetch(store_id))
-  @brand = Brand.create({:shoe_name => shoe_name, :store_id => store_id})
   erb(:store_info)
 end
 
@@ -40,6 +32,15 @@ patch("/stores/:id/edit") do
   @store = Store.update({:store_name => store_name})
   @stores = Store.all()
   erb(:stores_page)
+end
+
+
+post("/brands")do
+  shoe_name = params.fetch("shoe_name")
+  store_id = params.fetch("store_id").to_i()
+  @store = Store.find(params.fetch(store_id))
+  @brand = Brand.create({:shoe_name => shoe_name, :store_id => store_id})
+  erb(:store_info)
 end
 
 delete("/stores/:id/edit") do
---
+++ spec/spec_helper.rb
@@ -0,0 +1,12 @@
+require("rspec")
+require("pg")
+require("sinatra/activerecord")
+require("task")
+
+RSpec.configure do |config|
+  config.after(:each) do
+    Store.all().each() do |store|
+      store.destroy()
+    end
+  end
+end
